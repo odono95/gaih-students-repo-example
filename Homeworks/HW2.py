2bcec71123e6ef0143160c705166841de391b4b6
Ege Çıtak <egecitak01@gmail.com>
2021-03-14 22:43:07 +0300

    Add files via upload

---
+++ Homeworks/HW2.py
@@ -0,0 +1,37 @@
+cv1 = {
+    "FirstName": "Ege",
+    "LastName": "Çýtak",
+    "PhoneNumber": 31254687,
+    "Applied": False
+    }
+cv2 = {"FirstName": "Kaan",
+    "LastName": "Topla",
+    "PhoneNumber": 312254687,
+    "Applied": True
+    }
+
+cv3 = {
+    "FirstName": "Ahmet",
+    "LastName": "Dayý",
+    "PhoneNumber": 3221254687,
+    "Applied": True
+    }
+
+cv4 = {
+    "FirstName": "Yalçýn",
+    "LastName": "Ýkili",
+    "PhoneNumber": 3125422687,
+    "Applied": False
+    }
+
+cv5 = {
+    "FirstName": "Vehbi",
+    "LastName": "Koç",
+    "PhoneNumber": 312541211687,
+    "Applied": True
+    }
+
+cvList = [cv1, cv2, cv3, cv4, cv5]
+
+for i in cvList:
+    print(i)
